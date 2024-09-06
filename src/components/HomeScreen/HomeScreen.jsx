e806c3888d467c06a1785312b6a9b6418da8ac4f
arjit88 <arjitanand88@gmail.com>
2024-09-07 03:03:34 +0530

    Make the banner section

--- src/components/HomeScreen/HomeScreen.jsx
+++ src/components/HomeScreen/HomeScreen.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./HomeScreen.css";
 import Nav from "../Navbar/Nav";
+import Banner from "../Banner/Banner";
 
 const HomeScreen = () => {
   return (
     <div className="homeScreen">
       <Nav />
+      <Banner />
     </div>
   );
 };
