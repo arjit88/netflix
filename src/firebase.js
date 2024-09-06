e3b554acd7f70ee9b9e51280e50ae55226a24e12
arjit88 <arjitanand88@gmail.com>
2024-09-06 20:01:45 +0530

    Make the project with vite and install firebase and redux-toolkit

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBU9UJOAvsz7aRb1sARKoOMBp05S0Y_a0A",
+  authDomain: "netflix-clone-af892.firebaseapp.com",
+  projectId: "netflix-clone-af892",
+  storageBucket: "netflix-clone-af892.appspot.com",
+  messagingSenderId: "364546604243",
+  appId: "1:364546604243:web:9f520e8b7ddf7f8841e826",
+};
+
+const app = initializeApp(firebaseConfig);
