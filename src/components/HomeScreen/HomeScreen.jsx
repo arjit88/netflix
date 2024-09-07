5d618e3ed9a17fb7a6db3734ccc1fd6e3143d40d
arjit88 <arjitanand88@gmail.com>
2024-09-07 18:01:52 +0530

    Make the row section

--- src/components/HomeScreen/HomeScreen.jsx
+++ src/components/HomeScreen/HomeScreen.jsx
@@ -2,12 +2,26 @@ import React from "react";
 import "./HomeScreen.css";
 import Nav from "../Navbar/Nav";
 import Banner from "../Banner/Banner";
+import Row from "../Row/Row";
+import requests from "../../config/Requests";
 
 const HomeScreen = () => {
   return (
     <div className="homeScreen">
       <Nav />
       <Banner />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorroMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 };
--- src/config/Requests.js
+++ src/config/Requests.js
@@ -5,10 +5,10 @@ const requests = {
   fetchNetflixOriginals: `/discover/tv?api_key=${"c4d2f5db860396b544127ac219cadde5"}&with_network=213`,
   fetchTopRated: `/movie/top_rated?api_key=${"c4d2f5db860396b544127ac219cadde5"}&language=en-US`,
   fetchActionMovies: `/discover/movie?api_key=${"c4d2f5db860396b544127ac219cadde5"}&with_genres=28`,
-  fetchComedyMovies: `/discover/movies?api_key=${"c4d2f5db860396b544127ac219cadde5"}&with_genres=35`,
-  fetchHorroMovies: `/discover/movies?api_key=${"c4d2f5db860396b544127ac219cadde5"}&with_genres=27`,
-  fetchRomanceMovies: `/discover/movies?api_key=${"c4d2f5db860396b544127ac219cadde5"}&with_genres=10749`,
-  fetchDocumentaries: `/discover/movies?api_key=${"c4d2f5db860396b544127ac219cadde5"}&with_genres=99`,
+  fetchComedyMovies: `/discover/movie?api_key=${"c4d2f5db860396b544127ac219cadde5"}&with_genres=35`,
+  fetchHorroMovies: `/discover/movie?api_key=${"c4d2f5db860396b544127ac219cadde5"}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${"c4d2f5db860396b544127ac219cadde5"}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${"c4d2f5db860396b544127ac219cadde5"}&with_genres=99`,
 };
 
 export default requests;
